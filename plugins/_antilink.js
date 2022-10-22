6309a4aadff4803052cf88902775eccdd53b9dd3
Bk-SaD <82702287+Bk-SaD@users.noreply.github.com>
2022-10-22 16:31:57 -0400

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -15,11 +15,11 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
-await conn.sendButton(m.chat, `*𝐀𝐍𝐓𝐈~𝐋𝐈𝐍𝐊𝐒*\n*ᵁˢᵘᵃʳⁱᵒ:* ${await this.getName(m.sender)}\n*ᴬᶜᶜⁱᵒⁿ:* Ban🚫*${isBotAdmin ? '' : '\n\n*❗ ᴺᵒ ʰᵃᵍᵒ ᵐⁱˡᵃᵍʳᵒ.. ⁿᵒ ˢᵒʸ ᵃᵈᵐⁱⁿ`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂'/claimloli'], m)    
+await conn.sendButton(m.chat, `*𝐀𝐍𝐓𝐈~𝐋𝐈𝐍𝐊𝐒*\n*ᵁˢᵘᵃʳⁱᵒ:* ${await this.getName(m.sender)}\n*ᴬᶜᶜⁱᵒⁿ:* Ban🚫*${isBotAdmin ? '' : '\n\n*❗ ᴺᵒ ʰᵃᵍᵒ ᵐⁱˡᵃᵍʳᵒ.. ⁿᵒ ˢᵒʸ ᵃᵈᵐⁱⁿ*`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂'/claimloli'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')
+} else if (!bot.restrict) return m.reply('*❗ 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')
 }
 return !0
 }
